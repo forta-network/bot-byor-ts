e0a6e8a0f272abdbe32458ecd709ca985a11b472
ali <aliomerkeser@gmail.com>
2023-10-24 08:29:29 +0300

    add jwt

--- src/storage.ts
+++ src/storage.ts
@@ -1,4 +1,6 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from 'axios';
+
+import * as forta_agent from 'forta-agent';
 
 const owner_db = 'https://research.forta.network/database/owner/';
 
@@ -7,9 +9,21 @@ interface Secrets {
 		// Add other properties from your secrets.json if needed
 }
 
+async function getToken(): Promise<string> {
+		const tk = await forta_agent.fetchJwt({});
+		return `Bearer ${tk}`;
+}
+
 async function loadSecrets(key: string): Promise<Secrets> {
 		try {
-				const response = await axios.get(`${owner_db}${key}`);
+				const token = await getToken();
+				const headers: AxiosRequestConfig = {
+						headers: {
+								Authorization: token,
+						},
+				};
+
+				const response = await axios.get(`${owner_db}${key}`, headers);
 				if (response.status === 200) {
 						return response.data as Secrets;
 				} else {
